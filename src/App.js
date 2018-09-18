f772f493ad2d803ea6352c0640caa4ec0c276c5c
baurine <2008.hbl@gmail.com>
2018-09-18 15:07:26 +0800

    Add Collapse2 to demo support links in collpase head and body

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Collapse1 from './Collapse1';
+import Collapse2 from './Collapse2';
 
 class App extends Component {
   render() {
@@ -12,16 +13,16 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div className="App-body">
-          <a href="https://github.com/baurine/react-Collapse1">Go to GitHub</a>
-          <p></p>
+          <a href="https://github.com/baurine/react-collapse">Go to GitHub</a>
+         <p></p>
 
           <Collapse1/>
-          <Collapse1/>
+          <Collapse2/>
 
           <p>Auto Collapse1</p>
           <div>
             <Collapse1 autoCollapse={true}/>
-            <Collapse1 autoCollapse={true}/>
+            <Collapse2 autoCollapse={true}/>
           </div>
         </div>
       </div>
